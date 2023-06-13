248083bf4024f161b209788f701e484d729d64bf
chamatung <ckguswls312@gmail.com>
2023-06-13 10:51:18 +0900

    프로젝트 생성

---
+++ src/main/java/com/tour/kuma/TourkumaApplication.java
@@ -0,0 +1,13 @@
+package com.tour.kuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourkumaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourkumaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tour/kuma/TourkumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.kuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourkumaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
